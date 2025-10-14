bfc4998b74b00712702cb92220addbc0abb78199
baolingxiao <168592462+baolingxiao@users.noreply.github.com>
2025-10-14 10:33:35 -0400

    Refactor age_checker to return messages instead of print

--- age_checker.py
+++ age_checker.py
@@ -1,9 +1,9 @@
 def age_checker(a:int):
     if a >= 18:
-        print("You can vote")
+        return "You can vote"
     else :
-        print("Too young to vote.")
+        return "Too young to vote."
 
 a = int(input("Enter your age"))
 
-age_checker(a)
+print(age_checker(a))
